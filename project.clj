355e1de7c4ce0640205c32c06d88f83eb93de535
buhos_000 <albrecht.allenstein@gmail.com>
2014-02-04 02:26:14 +0100

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject flacon-clj "0.1.0-SNAPSHOT"
+  :description "A Clojure wrapper for Falcon fast FIX implementation."
+  :url "https://github.com/denarius-exchange/falcon-clj"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"})
