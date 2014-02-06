2f2dc8bbaaf8fbba5295b557ffdce93ebf6208d0
Javier <Javier@bebote>
2014-02-06 15:08:08 +0100

    Updated project with dependencies

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "A Clojure wrapper for Falcon fast FIX implementation."
   :url "https://github.com/denarius-exchange/falcon-clj"
   :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"})
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :resource-paths ["lib/falcon-0.1.0-master-SNAPSHOT-jar-with-dependencies.jar"])
